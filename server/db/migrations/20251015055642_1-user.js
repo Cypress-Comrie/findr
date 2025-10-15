085564fe0d81ec28b16bee5b3628c77caf65e612
Cypress Comrie <comrie1.cc@gmail.com>
2025-10-15 19:05:42 +1300

    created users table

--- server/db/migrations/20251015055642_1-user.js
+++ server/db/migrations/20251015055642_1-user.js
@@ -2,10 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export function up(knex) {}
+export function up(knex) {
+  return knex.schema.table('users', (table) => {
+    table.string('id').primary()
+    table.string('username')
+    table.string('full_name')
+    table.string('email').unique()
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export function down(knex) {}
+export function down(knex) {
+  return knex.schema.dropTable('users')
+}
