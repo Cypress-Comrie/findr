62c3e1270cff3001313c871b1548de72728fcf6e
Cypress Comrie <comrie1.cc@gmail.com>
2025-10-16 20:00:14 +1300

    created matches table

---
+++ server/db/migrations/20251016065721_4-matches.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.table('matches', (table) => {
+    table.string('id').primary()
+    table.string('relationship_id')
+    table.string('movie_id')
+    table.boolean('watched').defaultTo('false')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {}
