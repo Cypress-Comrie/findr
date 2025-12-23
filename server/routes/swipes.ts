7115ab4f90d67b467b31e9f6a6a2654fa85724cf
cypress <comrie1.cc@gmail.com>
2025-12-24 11:48:07 +1300

    added match checks into swipe post route

--- server/routes/swipes.ts
+++ server/routes/swipes.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import * as db from '../db/swipes'
+import * as matchesDb from '../db/matches'
+import { matchRoutes } from 'react-router'
 
 const router = express.Router()
 
@@ -17,11 +19,32 @@ router.get('/:userId', async (req, res) => {
 
 // gets the data from your swipes Example: your userId, the MovieId, True or False on the liked section
 // SwipeData sections is for grabbing the data we need from up above and then filling it out with the new swipe
+// now checks to see if your matches sync up with partners if true goes into shared if not stays in personal
 router.post('/', async (req, res) => {
   try {
     const SwipeData = { ...req.body }
     const newSwipe = await db.createSwipe(SwipeData)
-    res.status(201).json(newSwipe)
+    if (SwipeData.liked && SwipeData.relationship_id) {
+      const isMatch = await db.checkForMatch(
+        SwipeData.userId,
+        SwipeData.movie_id,
+        SwipeData.relationship_id,
+      )
+
+      if (isMatch) {
+        const match = await matchesDb.createMatches(
+          SwipeData.relationship_id,
+          SwipeData.movie_id,
+        )
+        return res.status(201).json({
+          swipe: newSwipe,
+          match: match,
+          isMatch: true,
+        })
+      }
+    }
+
+    res.status(201).json({ swipe: newSwipe, isMatch: false })
   } catch (error) {
     console.log(error)
     res.status(500).json({ error: 'something went wrong with swipe' })
