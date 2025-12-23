dd296eb2cced9eec376fdad372fd82afc8ecd07e
cypress <comrie1.cc@gmail.com>
2025-12-23 17:30:53 +1300

    created post route for swipes

--- server/routes/swipes.ts
+++ server/routes/swipes.ts
@@ -4,9 +4,7 @@ import * as db from '../db/swipes'
 const router = express.Router()
 // POST /api/swipes - Save a swipe (userId, movieId, liked)
 
-// GET /api/swipes/:userId - Get user's personal watchlist (all their liked movies)
-
-// Getting all users personal watchlist
+// Getting all users personal watchlisted movies
 router.get('/:userId', async (req, res) => {
   try {
     const user_id = Number(req.params.userId)
@@ -17,4 +15,17 @@ router.get('/:userId', async (req, res) => {
     res.status(500).json({ error: 'failed to get your watchlist' })
   }
 })
+
+// gets the data from your swipes Example: your userId, the MovieId, True or False on the liked section
+router.post('/', async (req, res) => {
+  try {
+    const SwipeData = { ...req.body }
+    const newSwipe = await db.createSwipe(SwipeData)
+    res.status(201).json(newSwipe)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'something went wrong with swipe' })
+  }
+})
+
 export default router
