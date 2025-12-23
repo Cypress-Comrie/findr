ea6f98ebb4b2b800bf5b1e38ca871d7bd8217aa8
cypress <comrie1.cc@gmail.com>
2025-12-24 10:54:19 +1300

    added in some comments

--- server/db/matches.ts
+++ server/db/matches.ts
@@ -3,6 +3,7 @@ import { Swipe, SwipeData } from '../../models/swipes'
 import { MovieData, Movie } from '../../models/movies'
 import { Match, MatchData } from '../../models/matches'
 
+// merges togther the movies and matches collums then provides the movies when the relationship id matches
 export async function GetMatches(relationship_id: number): Promise<Match[]> {
   return await db('matches')
     .join('movies', 'matches.movie_id', 'movie.id')
--- server/routes/matches.ts
+++ server/routes/matches.ts
@@ -3,6 +3,7 @@ import * as db from '../db/matches'
 
 const router = express.Router()
 
+// Get route gets the users relationship id and returns all the movies in their couple
 router.get('/:relationshipId', async (req, res) => {
   try {
     const relationship_id = Number(req.params.relationshipId)
