2ade0e89f2aa34458c565eb4c9eda226bacd4926
cypress <comrie1.cc@gmail.com>
2025-12-24 10:23:01 +1300

    fixed promise for getUserWatchlist

--- server/db/swipes.ts
+++ server/db/swipes.ts
@@ -1,5 +1,5 @@
 import { Swipe, SwipeData } from '../../models/swipes'
-import { Movies, MovieData } from '../../models/movies'
+import { MovieData, Movie } from '../../models/movies'
 import db from './connection'
 
 // creates swipe info for the database
@@ -17,7 +17,7 @@ export async function createSwipe(swipeData: SwipeData): Promise<Swipe> {
 
 // gets the movies from a user watchlist. joins the movies and the swipes tables to see when a user swiped on a movie
 // then gives back the movie with all its info
-export async function getUserWatchList(user_id: number): Promise<Swipe[]> {
+export async function getUserWatchList(user_id: number): Promise<Movie[]> {
   return await db('swipes')
     .join('movies', 'swipes.movie_id', 'movies.id')
     .where({ 'swipes.user_id': user_id, 'swipes.liked': true })
