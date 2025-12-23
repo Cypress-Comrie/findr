13ae723d9b308da43fddf6cb63122085dac80afb
cypress <comrie1.cc@gmail.com>
2025-12-24 11:36:59 +1300

    created createMatch db fn

--- server/db/matches.ts
+++ server/db/matches.ts
@@ -2,6 +2,7 @@ import db from './connection'
 import { Swipe, SwipeData } from '../../models/swipes'
 import { MovieData, Movie } from '../../models/movies'
 import { Match, MatchData } from '../../models/matches'
+import { Relationship, RelationshipData } from '../../models/relationships'
 
 // merges togther the movies and matches collums then provides the movies when the relationship id matches
 export async function GetMatches(relationship_id: number): Promise<Match[]> {
@@ -9,4 +10,30 @@ export async function GetMatches(relationship_id: number): Promise<Match[]> {
     .join('movies', 'matches.movie_id', 'movie.id')
     .where({ 'matches.relationship_id ': relationship_id })
     .select('movies.*')
+}
+
+export async function createMatches(
+  relationship_id: number,
+  movie_id: number,
+): Promise<Match> {
+  const existing = await db('matches')
+    .where({
+      relationship_id: relationship_id,
+      movie_id: movie_id,
+    })
+    .first()
+
+  if (existing) {
+    return existing
+  }
+
+  const [match] = await db('matches')
+    .insert({
+      relationship_id: relationship_id,
+      movie_id: movie_id,
+      watched: false,
+    })
+    .returning('*')
+
+  return match
 }
