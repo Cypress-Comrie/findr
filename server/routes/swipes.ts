50e29e22364e39d1dd9519970c1d7b6a4d953c8d
cypress <comrie1.cc@gmail.com>
2025-12-23 17:02:10 +1300

    finished auth, started on swipes checks for partners

--- server/routes/swipes.ts
+++ server/routes/swipes.ts
@@ -1,2 +1,7 @@
-import { Router } from 'express'
-import * as db from '../db/swipes.ts'
+import express from 'express'
+import * as db from '../db/swipes'
+
+const router = express.Router()
+// POST /api/swipes - Save a swipe (userId, movieId, liked)
+// GET /api/swipes/:userId - Get user's personal watchlist (all their liked movies)
+export default router
