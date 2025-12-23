33f9351fd1e41c90c66a4bb47a7d31ecef36de9a
cypress <comrie1.cc@gmail.com>
2025-12-24 10:39:58 +1300

    created db fn for getting matches for couples

---
+++ server/db/matches.ts
@@ -0,0 +1,11 @@
+import db from './connection'
+import { Swipe, SwipeData } from '../../models/swipes'
+import { MovieData, Movie } from '../../models/movies'
+import { Match, MatchData } from '../../models/matches'
+
+export async function GetMatches(relationship_id: number): Promise<Match[]> {
+  return await db('matches')
+    .join('movies', 'matches.movie_id', 'movie.id')
+    .where({ 'matches.relationship_id ': relationship_id })
+    .select('movies.*')
+}
