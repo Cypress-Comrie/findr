8c7a875424d195b553f691eabe6187c5c4dd64f2
cypress <comrie1.cc@gmail.com>
2025-12-28 16:26:12 +1300

    created a post route in users

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -5,8 +5,19 @@ const router = express.Router()
 
 router.post('/auth0', async (req, res) => {
   try {
-    const user = await connection('users')
-    .where({auth_id})
+    const { auth0_id, email, name } = req.body
+    let user = await connection('users').where({ auth0_id }).first()
+
+    if (!user) {
+      const [newUser] = await connection('users')
+        .insert({ auth0_id, email, name })
+        .returning('*')
+      user = newUser
+    }
+    res.json(user)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Failed to Get/Make user' })
   }
 })
 
